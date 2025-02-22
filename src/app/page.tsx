3ae527b089144f047c002f173ed087c46359c631
GeneralErdon <leandrofermin@gmail.com>
2025-02-21 21:02:48 -0400

    mejoras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Layout from "@/components/Layout/Layout";
+import HeroSection from "@/components/Portfolio/HeroSection";
 
 
 export default function Home() {
   return (
     <Layout >
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold">Hello, World!</h1>
-        <p className="mt-4">
-          This is a Next.js starter template with Tailwind CSS and TypeScript.
-        </p>
-      </div>
+      <HeroSection />
     </Layout>
   );
 }
